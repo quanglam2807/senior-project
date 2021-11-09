519c025886425ee1ca427e2aaa33b5c33ce1f4a1
Quang Lam <quang.lam2807@gmail.com>
2021-11-09 14:27:20 -0600

    Add PWA support

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import './firebase-app';
 
 import App from './components/App';
+
+serviceWorkerRegistration();
 
 ReactDOM.render(
   <React.StrictMode>
