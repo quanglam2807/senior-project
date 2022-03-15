e61140c9e1f128cff4ff22da8562c9729113a1cc
Quang Lam <quang.lam2807@gmail.com>
2022-03-15 15:29:01 -0500

    Add search support

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
 import cartReducer from './cart';
+import searchReducer from './search';
 
 const persistConfig = {
   key: 'root',
@@ -10,6 +12,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  search: searchReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
---
+++ src/reducers/search.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  query: [],
+};
+
+export const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    updateQuery: (state, action) => {
+      const newQuery = action.payload;
+      state.query = newQuery;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { updateQuery } = searchSlice.actions;
+
+export default searchSlice.reducer;
