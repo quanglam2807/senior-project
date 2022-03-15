40074ab1e5e3aadb86772412b8389574e6d7febf
Quang Lam <quang.lam2807@gmail.com>
2022-03-15 14:03:39 -0500

    Prevent normal users from accessing admin pages

--- src/components/layouts/LayoutAdmin/index.js
+++ src/components/layouts/LayoutAdmin/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 import TabBar from './TabBar';
 
-const LayoutUser = () => (
-  <>
-    <TabBar />
-    <Outlet />
-  </>
-);
+const LayoutAdmin = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
 
-export default LayoutUser;
+  useEffect(() => {
+    (async () => {
+      const { currentUser } = getAuth();
+      const docRef = doc(getFirestore(), 'users', currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      const data = docSnap.data();
+      setIsAdmin(data && data.role === 'admin');
+    })();
+  }, []);
+
+  if (!isAdmin) return 'Access denied';
+
+  return (
+    <>
+      <TabBar />
+      <Outlet />
+    </>
+  );
+};
+
+export default LayoutAdmin;
