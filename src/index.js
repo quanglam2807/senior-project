5ffc20453011e81612637b85f3468877c463583e
Quang Lam <quang.lam2807@gmail.com>
2022-03-15 12:00:46 -0500

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+import { store } from './reducers';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -16,7 +19,9 @@ import App from './components/App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
